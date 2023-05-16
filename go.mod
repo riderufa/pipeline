cc74939d426c64d459a3fb4e5d1058dd3944cb93
Garri <3434455@mail.ru>
2023-05-16 16:51:16 +0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module module_20
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
+)
+
+type RingBuffer struct {
+	array         []int
+	size          int
+	drainInterval time.Duration
+	pos           int
+	m             sync.Mutex
+}
+
+func NewRingBuffer(size int, drainInterval time.Duration) *RingBuffer {
+	return &RingBuffer{make([]int, size), size, drainInterval, -1, sync.Mutex{}}
+}
+
+func (r *RingBuffer) Push(e int) {
+	r.m.Lock()
+	defer r.m.Unlock()
+	if r.pos == r.size-1 {
+		for i := 1; i < r.size; i++ {
+			r.array[i-1] = r.array[i]
+		}
+	} else {
+		r.pos++
+	}
+	r.array[r.pos] = e
+}
+
+func (r *RingBuffer) Pop() (int, bool) {
+	if r.pos < 0 {
+		return 0, false
+	}
+	r.m.Lock()
+	defer r.m.Unlock()
+	var output = r.array[r.pos]
+	r.pos--
+	return output, true
+}
+
+func (r *RingBuffer) Get() []int {
+	if r.pos < 0 {
+		return nil
+	}
+	r.m.Lock()
+	defer r.m.Unlock()
+	var output = r.array[:r.pos+1]
+	r.pos = -1
+	return output
+}
+
+func read(input chan<- int, done chan bool) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		data := scanner.Text()
+		if strings.EqualFold(data, "exit") {
+			fmt.Println("The program has ended")
+			close(done)
+			return
+		}
+		i, err := strconv.Atoi(data)
+		if err != nil {
+			fmt.Println("The program only accepts integers")
+			continue
+		}
+		input <- i
+	}
+}
+
+func negativeFilter(input <-chan int, output chan<- int, done <-chan bool) {
+	for {
+		select {
+		case data := <-input:
+			if data >= 0 {
+				output <- data
+			}
+		case <-done:
+			return
+		}
+	}
+}
+
+func notDividedThreeFilter(input <-chan int, output chan<- int, done <-chan bool) {
+	for {
+		select {
+		case data := <-input:
+			if data%3 == 0 {
+				output <- data
+			}
+		case <-done:
+			return
+		}
+	}
+}
+
+func (r *RingBuffer) bufferStage(input <-chan int, output chan<- int, done <-chan bool) {
+	for {
+		select {
+		case data := <-input:
+			r.Push(data)
+		case <-time.After(r.drainInterval):
+			//for {
+			//	e, err := r.Pop()
+			//	if err {
+			//		return
+			//	}
+			//	output <- e
+			//}
+			a := r.Get()
+			if a != nil {
+				for _, e := range a {
+					output <- e
+				}
+			}
+		case <-done:
+			return
+		}
+	}
+}
+
+func main() {
+	input := make(chan int)
+	done := make(chan bool)
+	go read(input, done)
+
+	negativeFiltered := make(chan int)
+	go negativeFilter(input, negativeFiltered, done)
+
+	notDividedThreeFiltered := make(chan int)
+	go notDividedThreeFilter(negativeFiltered, notDividedThreeFiltered, done)
+
+	buffer := NewRingBuffer(10, 10*time.Second)
+	buffered := make(chan int)
+	go buffer.bufferStage(notDividedThreeFiltered, buffered, done)
+
+	for {
+		select {
+		case data := <-buffered:
+			fmt.Println("Processed data, ", data)
+		case <-done:
+			return
+		}
+	}
+}
